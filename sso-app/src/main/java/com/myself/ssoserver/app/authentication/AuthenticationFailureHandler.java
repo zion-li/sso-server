c9db1edca3ebfa43d501e32bc3c69474de0ae5ee
李佳 <zion.li@baifendian.com>
2019-08-21 18:57:07 +0800

    登录失败处理

--- sso-app/src/main/java/com/myself/ssoserver/app/authentication/AuthenticationFailureHandler.java
+++ sso-app/src/main/java/com/myself/ssoserver/app/authentication/AuthenticationFailureHandler.java
@@ -2,9 +2,13 @@ package com.myself.ssoserver.app.authentication;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myself.ssoserver.app.service.CustomerLoginLogService;
+import com.myself.ssoserver.properties.SecurityConstants;
 import com.myself.ssoserver.support.ApiResponse;
 import com.myself.ssoserver.support.StatusEnum;
+import com.myself.ssoserver.util.IpUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -29,10 +33,24 @@ public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailure
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private CustomerLoginLogService customerLoginLogService;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException e) throws IOException, ServletException {
-        logger.info("登录失败");
+
+        if (StringUtils.equals(request.getRequestURI(), SecurityConstants.DEFAULT_SIGN_IN_PROCESSING_URL_FORM)) {
+            String username = request.getParameter("username");
+            if (StringUtils.isNotBlank(username)) {
+                customerLoginLogService.recordLoginerror(request.getRequestURI(), username, IpUtil.getIpAddr(request));
+            }
+        } else if (StringUtils.equals(request.getRequestURI(), SecurityConstants.DEFAULT_SIGN_IN_PROCESSING_URL_MOBILE)) {
+            String mobile = request.getParameter(SecurityConstants.DEFAULT_PARAMETER_NAME_MOBILE);
+            if (StringUtils.isNotBlank(mobile)) {
+                customerLoginLogService.recordLoginerror(request.getRequestURI(), mobile, IpUtil.getIpAddr(request));
+            }
+        }
 
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         response.setContentType("application/json;charset=UTF-8");
