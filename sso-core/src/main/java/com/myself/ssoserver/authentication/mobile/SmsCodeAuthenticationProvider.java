7a9fdb2da523ae9d966996f2a998a58c0f8e1fdc
李佳 <zion.li@baifendian.com>
2019-08-28 10:36:14 +0800

    添加短信验证码用户锁定认证

--- sso-core/src/main/java/com/myself/ssoserver/authentication/mobile/SmsCodeAuthenticationProvider.java
+++ sso-core/src/main/java/com/myself/ssoserver/authentication/mobile/SmsCodeAuthenticationProvider.java
@@ -1,12 +1,16 @@
 package com.myself.ssoserver.authentication.mobile;
 
 import lombok.Data;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.SpringSecurityMessageSource;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsChecker;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.Assert;
 
 /**
  * 短信登录验证逻辑
@@ -16,12 +20,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * @Date 2019/1/30.
  */
 @Data
+@Slf4j
 public class SmsCodeAuthenticationProvider implements AuthenticationProvider {
 
     /**
      * 用户信息
      */
     private UserDetailsService userDetailsService;
+    protected MessageSourceAccessor messages = SpringSecurityMessageSource.getAccessor();
+    private UserDetailsChecker preAuthenticationChecks = new SmsCodeAuthenticationProvider.SmsCodePreAuthenticationChecks();
+    private UserDetailsChecker postAuthenticationChecks = new SmsCodeAuthenticationProvider.SmsCodePostAuthenticationChecks();
+
 
     /**
      * 进行身份认证的逻辑
@@ -34,11 +43,19 @@ public class SmsCodeAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
+        Assert.isInstanceOf(SmsCodeAuthenticationToken.class, authentication,
+            () -> messages.getMessage(
+                "SmsCodeAuthenticationProvider.onlySupports",
+                "Only SmsCodeAuthenticationProvider is supported"));
+
         SmsCodeAuthenticationToken authenticationToken = (SmsCodeAuthenticationToken) authentication;
         UserDetails user = userDetailsService.loadUserByUsername((String) authenticationToken.getPrincipal());
         if (user == null) {
             throw new InternalAuthenticationServiceException("无法通过手机号获取用户信息");
         }
+        preAuthenticationChecks.check(user);
+        postAuthenticationChecks.check(user);
+
         SmsCodeAuthenticationToken authenticationResult = new SmsCodeAuthenticationToken(user, user.getAuthorities());
         authenticationResult.setDetails(authentication.getDetails());
         return authenticationResult;
@@ -48,4 +65,47 @@ public class SmsCodeAuthenticationProvider implements AuthenticationProvider {
     public boolean supports(Class<?> authentication) {
         return SmsCodeAuthenticationToken.class.isAssignableFrom(authentication);
     }
+
+    private class SmsCodePreAuthenticationChecks implements UserDetailsChecker {
+        @Override
+        public void check(UserDetails user) {
+            if (!user.isAccountNonLocked()) {
+                log.debug("User account is locked");
+
+                throw new LockedException(messages.getMessage(
+                    "AbstractUserDetailsAuthenticationProvider.locked",
+                    "User account is locked"));
+            }
+
+            if (!user.isEnabled()) {
+                log.debug("User account is disabled");
+
+                throw new DisabledException(messages.getMessage(
+                    "AbstractUserDetailsAuthenticationProvider.disabled",
+                    "User is disabled"));
+            }
+
+            if (!user.isAccountNonExpired()) {
+                log.debug("User account is expired");
+
+                throw new AccountExpiredException(messages.getMessage(
+                    "AbstractUserDetailsAuthenticationProvider.expired",
+                    "User account has expired"));
+            }
+        }
+    }
+
+    private class SmsCodePostAuthenticationChecks implements UserDetailsChecker {
+        @Override
+        public void check(UserDetails user) {
+            if (!user.isCredentialsNonExpired()) {
+                log.debug("User account credentials have expired");
+
+                throw new CredentialsExpiredException(messages.getMessage(
+                    "AbstractUserDetailsAuthenticationProvider.credentialsExpired",
+                    "User credentials have expired"));
+            }
+        }
+    }
 }
+
